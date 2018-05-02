6838235cdc158bd1c3a7e8206ce1d6c8c2f9a8c8
Sankalp <sankalp2@illinois.edu>
2018-05-02 08:19:39 -0500

    MP7 Commit

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -3,6 +3,10 @@ package edu.illinois.cs.cs125.lab12;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,7 +17,6 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 /**
  * Main class for our UI design lab.
  */
@@ -24,6 +27,9 @@ public final class MainActivity extends AppCompatActivity {
     /** Request queue for our API requests. */
     private static RequestQueue requestQueue;
 
+    /** number of possible plants*/
+    private final int randNum = 50;
+
     /**
      * Run when this activity comes to the foreground.
      *
@@ -32,13 +38,18 @@ public final class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set up the queue for our API requests
         requestQueue = Volley.newRequestQueue(this);
-
-        setContentView(R.layout.activity_main);
-
         startAPICall();
+        setContentView(R.layout.activity_main);
+        Button getImage = (Button) findViewById(R.id.getImage);
+        getImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                startAPICall();
+            }
+        });
+
     }
 
     /**
@@ -50,20 +61,38 @@ public final class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Make a call to the weather API.
+     * Update the text
+     * @param response JSON from API
+     * @throws JSONException in case things don't work out
+     */
+    void updateText(final JSONObject response) throws JSONException {
+        String name = response.getString("name").toString();
+        TextView text = (TextView) findViewById(R.id.TextBox);
+        String output = "Name: " + name + "\nRotation Period: " + response.getString("rotation_period").toString()
+                + "\nOrbital Period: " + response.getString("orbital_period").toString() + "\nDiameter: " +
+                response.getString("diameter").toString() + "\nClimate: " + response.getString("climate").toString() +
+                "\nGravity: " + response.getString("gravity").toString() + "\nPopulation: " +
+                response.getString("population").toString() + "\n\n";
+
+                text.setText(output);
+    }
+
+    /**
+     * Make a call to the API.
      */
     void startAPICall() {
+
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "http://api.openweathermap.org/data/2.5/weather?zip=61820,us&appid="
-                            + BuildConfig.API_KEY,
+                    "https://swapi.co/api/planets/" + (int) (Math.random() * randNum) + "/",
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
                             try {
                                 Log.d(TAG, response.toString(2));
+                                updateText(response);
                             } catch (JSONException ignored) { }
                         }
                     }, new Response.ErrorListener() {
